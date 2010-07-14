a86c7c2bd2e1a6aa1d09d23495642ddab267d280
Robert Burkhead <rmburkhead@gmail.com>
2010-07-14 01:02:02 +0000

    Implement the Account API to display a link to the view function of the module to allow users to manage the OpenIDs associated with their account.

---
+++ OpenID/lib/OpenID/Api/Account.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Zikula Application Framework
+ *
+ * @copyright 2001 Zikula Development Team
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package Zikula
+ * @subpackage OpenID
+ * @version $Id$
+ * @link http://www.zikula.org
+ */
+
+/**
+ * The Account API provides links for modules on the "user account page"; this
+ * class provides those links for the Users module.
+ *
+ * @package Zikula
+ * @subpackage OpenID
+ */
+class OpenID_Api_Account extends Zikula_Api
+{
+    /**
+     * Return an array of items to show in the the user's account panel.
+     *
+     * @return array Indexed array of items.
+     */
+    public function getAll()
+    {
+        $items = array();
+
+        $items[] = array(
+            'url' => ModUtil::url('OpenID', 'user', 'view'),
+            'module' => 'OpenID',
+            'icon' => 'large/openid-icon.png',
+            'title' => $this->__('OpenID manager'),
+        );
+
+        // Return the items
+        return $items;
+    }
+}
